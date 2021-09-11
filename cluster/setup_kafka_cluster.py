8eb04aa7bc8b4dbef15ed2e5f44af3c2952c74be
gazwald <gary.brandon@gmail.com>
2021-09-11 10:59:04 +1000

    Switching from Boto3 SSM to CDK SSM

--- cluster/setup_kafka_cluster.py
+++ cluster/setup_kafka_cluster.py
@@ -1,46 +1,51 @@
 #!/usr/bin/env python3
 import os
-import boto3
 from aws_cdk import core as cdk
 
 import aws_cdk.aws_ec2 as ec2
 import aws_cdk.aws_msk as msk
+import aws_cdk.aws_ssm as ssm
 
 
 class KafkaesqueStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        vpc = ec2.Vpc.from_lookup(self, "SharedVPC", vpc_id=self.get_vpc_id())
-
+        vpc_id = ssm.StringParameter.value_for_string_parameter(self, "/common/shared_vpc_id")
+        vpc = ec2.Vpc.from_lookup(self, "SharedVPC", vpc_id=vpc_id)
         isolated_subnets = ec2.SubnetSelection(subnets=vpc.isolated_subnets)
-        instance_type = ec2.InstanceType.of(
-            ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.SMALL
-        )
-        storage = msk.EbsStorageInfo(volume_size=100)
 
-        cluster = msk.Cluster(
-            self,
-            "cluster",
-            cluster_name="kafkaesque",
-            instance_type=instance_type,
-            kafka_version=msk.KafkaVersion.V2_8_0,
-            vpc=vpc,
-            vpc_subnets=isolated_subnets,
-            ebs_storage_info=storage,
-            removal_policy=cdk.RemovalPolicy.DESTROY,
-        )
+        cluster = self.setup_cluster(vpc, isolated_subnets)
+        bastion = self.setup_bastion(vpc, isolated_subnets)
 
         print(cluster.bootstrap_brokers)
 
         # cluster.add_user("test_user")
 
-    def get_vpc_id(self):
-        client = boto3.client("ssm", region_name="ap-southeast-2")
-        r = client.get_parameter(Name="/common/shared_vpc_id")
+    def setup_cluster(self, vpc: ec2.Vpc, subnets: ec2.SubnetSelection) -> msk.Cluster:
+        instance_type = ec2.InstanceType.of(
+            ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.SMALL
+        )
+        storage = msk.EbsStorageInfo(volume_size=100)
 
-        if "Parameter" in r.keys():
-            return r["Parameter"]["Value"]
+        return msk.Cluster(
+            self,
+            "cluster",
+            cluster_name="kafkaesque",
+            instance_type=instance_type,
+            kafka_version=msk.KafkaVersion.V2_8_0,
+            vpc=self.vpc,
+            vpc_subnets=subnets,
+            ebs_storage_info=storage,
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+        )
+
+    def setup_bastion(
+        self, vpc: ec2.Vpc, subnets: ec2.SubnetSelection
+    ) -> ec2.BastionHostLinux:
+        return ec2.BastionHostLinux(
+            self, "BastionHost", vpc=vpc, subnet_selection=subnets
+        )
 
 
 def main():
