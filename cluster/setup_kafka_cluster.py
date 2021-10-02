8ce92c023e97db41d48db44c89e78bdb777e1232
gazwald <gary.brandon@gmail.com>
2021-10-02 10:14:12 +1000

    Cleaned up kafka cluster creation

--- cluster/setup_kafka_cluster.py
+++ cluster/setup_kafka_cluster.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 import os
-from aws_cdk import core as cdk
+import boto3
 
 import aws_cdk.aws_ec2 as ec2
 import aws_cdk.aws_msk as msk
-import aws_cdk.aws_ssm as ssm
+from aws_cdk import core as cdk
 
 
 class KafkaesqueStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        vpc_id = ssm.StringParameter.value_for_string_parameter(self, "/common/shared_vpc_id")
+        vpc_id = self.get_ssm("/common/shared_vpc_id")
         vpc = ec2.Vpc.from_lookup(self, "SharedVPC", vpc_id=vpc_id)
         isolated_subnets = ec2.SubnetSelection(subnets=vpc.isolated_subnets)
 
@@ -22,11 +22,16 @@ class KafkaesqueStack(cdk.Stack):
 
         # cluster.add_user("test_user")
 
-    def setup_cluster(self, vpc: ec2.Vpc, subnets: ec2.SubnetSelection) -> msk.Cluster:
-        instance_type = ec2.InstanceType.of(
-            ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.SMALL
-        )
-        storage = msk.EbsStorageInfo(volume_size=100)
+    def setup_cluster(
+        self,
+        vpc: ec2.Vpc,
+        subnets: ec2.SubnetSelection,
+        instance_class: ec2.InstanceClass = ec2.InstanceClass.BURSTABLE3,
+        instance_type: ec2.InstanceSize = ec2.InstanceSize.SMALL,
+        volume_size: int = 100,
+    ) -> msk.Cluster:
+        instance_type = ec2.InstanceType.of(instance_class, instance_type)
+        storage = msk.EbsStorageInfo(volume_size=volume_size)
 
         return msk.Cluster(
             self,
@@ -34,7 +39,7 @@ class KafkaesqueStack(cdk.Stack):
             cluster_name="kafkaesque",
             instance_type=instance_type,
             kafka_version=msk.KafkaVersion.V2_8_0,
-            vpc=self.vpc,
+            vpc=vpc,
             vpc_subnets=subnets,
             ebs_storage_info=storage,
             removal_policy=cdk.RemovalPolicy.DESTROY,
@@ -46,6 +51,13 @@ class KafkaesqueStack(cdk.Stack):
         return ec2.BastionHostLinux(
             self, "BastionHost", vpc=vpc, subnet_selection=subnets
         )
+
+    @staticmethod
+    def get_ssm(parameter: str) -> str:
+        client = boto3.client("ssm", region_name="ap-southeast-2")
+        r = client.get_parameter(Name=parameter)
+        if "Parameter" in r.keys():
+            return r["Parameter"]["Value"]
 
 
 def main():
