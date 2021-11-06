c45f5517c759feed5edbc1923c728061ca6bea74
gazwald <gary.brandon@gmail.com>
2021-11-06 11:51:14 +1100

    Dockerfile linting

--- scripts/lint_check.sh
+++ scripts/lint_check.sh
@@ -10,3 +10,10 @@ isort --check --extend-skip cdk.out .
 echo
 echo "+++ Checking with MyPy +++"
 mypy .
+echo
+echo "+++ Checking Dockerfiles +++"
+for dockerfile in $(find -type f -name Dockerfile ! -path "*cdk.out*"); do
+  echo "+++ Checking $dockerfile +++"
+  docker run --rm -i hadolint/hadolint < $dockerfile
+  echo
+done
